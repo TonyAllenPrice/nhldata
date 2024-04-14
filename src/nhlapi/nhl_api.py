14b7439854497c785d47cf2f7619aa98554927c1
Tony Allen Price <tony@tonyallenprice.com>
2024-04-13 23:47:32 -0500

    Refactoring nhl_api.py for simplicity

--- src/nhlapi/nhl_api.py
+++ src/nhlapi/nhl_api.py
@@ -1,6 +1,5 @@
 from connector import Wrapper
 from typing import List, Dict
-import itertools
 
 class NHLApi:
     def __init__(self,ver: str = 'v1', lang: str = 'en', ssl_verify: bool = True):
@@ -9,15 +8,14 @@ class NHLApi:
     def active_teams(self, season:str) -> List:
         endpoint = 'goalie/summary'
         ep_params = {
-                'limit':'-1',
-                'cayenneExp':'',
-                'seasonId':season
-            }
-        data = self._wrapper.get(type='stats', endpoint = endpoint, ep_params = ep_params)
+            'limit': '-1',
+            'cayenneExp': '',
+            'seasonId': season
+        }
+        data = self._wrapper.get(type='stats', endpoint=endpoint, ep_params=ep_params)
         goalies = data['data']
-        teams = [g['teamAbbrevs'] for g in goalies]
-        teams = list(set(teams))
-        return [t for t in teams if len(t) == 3]
+        teams = {g['teamAbbrevs'] for g in goalies if len(g['teamAbbrevs']) == 3}
+        return list(teams)
 
     def seasons(self) -> List:
         return self._wrapper.get(type='web',endpoint='season')
@@ -36,27 +34,29 @@ class NHLApi:
 
     def roster(self,team: str, season: str) -> List[Dict]:
         data = self._wrapper.get(type='web', endpoint=f'roster/{team}/{season}')
-        players = [data[pos] for pos in data]
-        changes = ['firstName','lastName','birthCity','birthStateProvince'] # These 4 have nested natures with FR translations. We are pulling just English/Default.
-        players = list(itertools.chain.from_iterable(players))
-        for player, change in itertools.product(players, changes):
-            if change in player: # Not every player has a State/Province, but this allows one loop to do all 4 changes.
-                player[change] = player[change]['default']
+        changes = ['firstName', 'lastName', 'birthCity', 'birthStateProvince']
+        players = [player for pos in data for player in data[pos]]
+        for player in players:
+            for change in changes:
+                if change in player:
+                    player[change] = player[change]['default']
         return players
       
     def stats_players(self,season:str,pos:str,total_players:int=1000) -> List[Dict]:
-        results = []
-        for x in range(0,total_players,100):
-            ep_params = {
-                'limit':'100',
-                'start':x,
-                'cayenneExp':'',
-                'seasonId':season
-            }
-            data = self._wrapper.get(type='stats',endpoint=f'{pos}/summary', ep_params=ep_params)
-            results.append(data['data'])
-        
-        return list(itertools.chain.from_iterable(results))
+        return [
+            player
+            for x in range(0, total_players, 100)
+            for player in self._wrapper.get(
+                type='stats',
+                endpoint=f'{pos}/summary',
+                ep_params={
+                    'limit': '100',
+                    'start': x,
+                    'cayenneExp': '',
+                    'seasonId': season
+                }
+            )['data']
+        ]
     
     def game_long_player(self,player_id:str,season:str,game_type:int) -> List[Dict]:
         return self._wrapper.get(type='web',endpoint=f'player/{player_id}/game-log/{season}/{game_type}')['gameLog']
