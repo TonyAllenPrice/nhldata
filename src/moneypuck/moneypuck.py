55dd37df55428060033b4e14c6822ab5d4d0204b
Tony Allen Price <tony@tonyallenprice.com>
2024-04-27 13:29:50 -0500

    Quick refactor.

--- src/moneypuck/moneypuck.py
+++ src/moneypuck/moneypuck.py
@@ -17,36 +17,27 @@ import zipfile
 
 BASE_URL = 'https://moneypuck.com/moneypuck/playerData/'
 
-def _fetch_file(base:str,endpoint:str,file_name:str):
+def _fetch_and_process_data(base:str, endpoint:str, file_name:str):
     url = f'{base}{endpoint}{file_name}.csv'
     r = requests.get(url)
-    return io.StringIO(r.text)
-
-def _process_file(base:str,endpoint:str,file_name:str):
-    data_stream = _fetch_file(base, endpoint, file_name)
+    data_stream = io.StringIO(r.text)
     return [dict(row.items()) for row in csv.DictReader(data_stream, skipinitialspace=True)]
 
-def season_stats(gametype:str,file_type:str,seasons:list):
-    res = []  
-    for season in seasons:
-        endpoint = f'seasonSummary/{season}/{gametype}/'
-        res.extend(_process_file(BASE_URL,endpoint,file_type))
-    return res
+def season_stats(gametype:str, file_type:str, seasons:list):
+    return [data for season in seasons for data in _fetch_and_process_data(BASE_URL, f'seasonSummary/{season}/{gametype}/', file_type)]
 
-def player_by_game(player_id:int,position:str,gametype:str):
-    endpoint = f'careers/gameByGame/{gametype}/{position}/'
-    return _process_file(BASE_URL,endpoint,player_id)
+def player_by_game(player_id:int, position:str, gametype:str):
+    return _fetch_and_process_data(BASE_URL, f'careers/gameByGame/{gametype}/{position}/', player_id)
 
-def team_stats(gametype:str,team:str):
-    endpoint = f'careers/gameByGame/{gametype}/teams/'
-    return _process_file(BASE_URL,endpoint,team)
+def team_stats(gametype:str, team:str):
+    return _fetch_and_process_data(BASE_URL, f'careers/gameByGame/{gametype}/teams/', team)
 
 def get_shots(season:int):
     r = requests.get(f'https://peter-tanner.com/moneypuck/downloads/shots_{season}.zip')
     with zipfile.ZipFile(io.BytesIO(r.content)) as z:
-        for zipfile in z.infolist():
-            with z.open(zipfile) as thefile:
-                yield zipfile.filename, thefile
+        for zip_info in z.infolist():
+            with z.open(zip_info) as thefile:
+                yield zip_info.filename, thefile
 
 if __name__ == '__main__':
     shots_2023 = get_shots(2023)
