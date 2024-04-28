214ebb1da15417d60505aafa54f0c94b999479b0
Tony Allen Price <tony@tonyallenprice.com>
2024-04-27 22:40:05 -0500

    Update to 0.0.1

--- setup.py
+++ setup.py
@@ -1,2 +1,16 @@
 from setuptools import setup
-setup(name='nhlstats')
+
+setup(
+    name='nhlstats',
+    version='0.0.1',
+    install_requires=[
+        'requests',
+        'csv',
+        'zipfile'
+    ],
+    author='Tony Price',
+    license='MIT',
+    packages=[
+        'nhlstats','nhlstats.moneypuck','nhlstats.nhl'
+    ]
+)
